62fbf57dd62522b23d8b671c741e5d58fd44ca1c
Jorge Guilherme <jorgekg3@gmail.com>
2019-08-22 11:31:33 -0300

    Update Database.class.php

--- backend/controller/Database.class.php
+++ backend/controller/Database.class.php
@@ -7,7 +7,7 @@ class DatabaseController {
   public function get_instance() {
     if (empty($this->instance)) {
       $this->instance =
-        new PDO('mysql:unix_socket=/tmp/mysql.sock;dbname=simple_email_subscribe', 'jorge', 'faccao12');
+        new PDO('mysql:unix_socket=/tmp/mysql.sock;dbname=simple_email_subscribe', 'jorge', '');
       $this->instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
     return $this->instance;
